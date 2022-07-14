2781fa00b3958761851b60a29e9e117b1ce540b9
Danilo Nogueira <nvdanilo@gmail.com>
2022-07-13 21:45:18 -0300

    Migrations

---
+++ laravel/database/migrations/2022_07_14_004106_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("device");
+            $table->string("brand");
+            $table->string("model");
+            $table->string("serial_number");
+            $table->text("accessories");
+            $table->text("reported_problem");
+            $table->text("service_description");
+            $table->string("status");
+            $table->double("price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
--- laravel/resources/views/home/contact.blade.php
+++ laravel/resources/views/home/contact.blade.php
@@ -1,5 +1,5 @@
 @extends("template.home")
-@section("title", "OS | Sobre")
+@section("title", "OS | Contato")
 @section("body")
     <h1>CONTATO</h1>
 @endsection
