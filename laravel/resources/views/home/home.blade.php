2f2176cb62bd1828a3351e8eef0a71b5251c3713
Danilo Nogueira <nvdanilo@gmail.com>
2022-08-04 02:01:19 -0300

    Auth ++

--- laravel/resources/views/home/home.blade.php
+++ laravel/resources/views/home/home.blade.php
@@ -1,5 +1,5 @@
 @extends("template.home")
 @section("title", "OS | Home")
 @section("body")
-    <h1>INÍCIO</h1>
+    <h1 class="d-grid gap-2 d-md-flex justify-content-md-center mt-5">BEM VINDO!</h1>
 @endsection
